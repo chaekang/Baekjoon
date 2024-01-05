23f1e9767ec66ee918d465466fad7dc87b1cd5d5
chaekang <herry612@naver.com>
2024-01-05 14:40:49 +0900

    1/4
    1/4

--- 9375.py
+++ 9375.py
@@ -3,11 +3,20 @@
 import sys
 
 n = int(sys.stdin.readline())
-cloth = dict()
 
 for i in range(n):
     m = int(sys.stdin.readline())
+    closet = dict()
     for j in range(m):
         clth, sort = sys.stdin.readline().split()
-        cloth[clth] = sort
+        if sort in closet:
+            closet[sort].append(clth)
+        else:
+            closet[sort] = [clth]
+
+    cnt = 1
+
+    for key in closet:
+        cnt *= len(closet[key]) + 1
         
+    print(cnt-1)
