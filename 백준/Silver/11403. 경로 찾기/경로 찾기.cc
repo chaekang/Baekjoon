ec1e4200eab726554214700b8bbbb7fbc0bd1316
송채강 <herry612@naver.com>
2025-04-08 01:23:34 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	int graph[101][101];
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> graph[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (graph[k][i] == 1 && graph[i][j] == 1) {
+					graph[k][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
