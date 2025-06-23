47a03e763767cd4d643178eeb933edbe8acf87c0
송채강 <herry612@naver.com>
2025-06-24 01:42:52 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int grape[10001];
+int dp[10001];
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> grape[i];
+	}
+
+	dp[0] = 0;
+	dp[1] = grape[1];
+	dp[2] = grape[1] + grape[2];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max({ dp[i - 3] + grape[i - 1] + grape[i], dp[i - 2] + grape[i], dp[i - 1] });
+	}
+
+	cout << dp[n];
+}
