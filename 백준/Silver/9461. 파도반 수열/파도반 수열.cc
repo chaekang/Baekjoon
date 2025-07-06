9c3a7f3c4926f4c1a73b1ede36eb27d8f0d80bf7
송채강 <herry612@naver.com>
2025-07-07 01:19:45 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -2,32 +2,21 @@
 
 using namespace std;
 
-long long dp[110];
-
-long long DP(int x) {
-	if (x <= 5) {
-		return dp[x];
-	}
-
-	for (int i = 6; i <= x; i++) {
-		dp[i] = dp[i - 1] + dp[i - 5];
-	}
-	return dp[x];
-}
-
 int main() {
 	int t;
+	long long dp[106];
 	cin >> t;
-	dp[1] = 1;
-	dp[2] = 1;
-	dp[3] = 1;
-	dp[4] = 2;
-	dp[5] = 2;
 
-	for (int i = 0; i < t; i++) {
+	dp[1] = dp[2] = dp[3] = 1;
+	dp[4] = dp[5] = 2;
+
+	for (int i = 6; i < 105; i++) {
+		dp[i] = dp[i - 1] + dp[i - 5];
+	}
+
+	while (t--) {
 		int n;
 		cin >> n;
-
-		cout << DP(n) << "\n";
+		cout << dp[n] << "\n";
 	}
 }
