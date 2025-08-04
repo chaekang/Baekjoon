ca6a489063b6e230be670ae8246edd8b0b69d454
송채강 <herry612@naver.com>
2025-08-04 11:07:50 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1001];
+
+int main() {
+	int x;
+	cin >> x;
+
+	dp[1] = 1;
+	dp[2] = 3;
+
+	for (int i = 3; i <= x; i++) {
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+	}
+
+	cout << dp[x];
+}
