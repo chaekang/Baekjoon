ea343031b90d3933bd1ff68e76e11ac95211b778
송채강 <herry612@naver.com>
2025-08-08 01:36:27 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int board[100][100];
+long long dp[100][100];
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] == 0) continue;
+
+			int jump = board[i][j];
+
+			if (i + jump < n) dp[i + jump][j] += dp[i][j];
+			if (j + jump < n) dp[i][j + jump] += dp[i][j];
+		}
+	}
+
+	cout << dp[n - 1][n - 1];
+}
