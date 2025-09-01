0ac5350b08dc0fa9b2e520148c201c8ab61c06cb
송채강 <herry612@naver.com>
2025-09-02 01:43:19 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	vector<int> b(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+	}
+	sort(a.begin(), a.end());
+	sort(b.rbegin(), b.rend());
+	
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += a[i] * b[i];
+	}
+	cout << ans;
+}
