ab18c79ac64b81f386f16491993ba5e2f43aa2ff
송채강 <herry612@naver.com>
2026-02-12 02:00:55 +0900

    [Gold V] Title: 물병, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n, k;
+	cin >> n >> k;
+
+	if (k >= n) {
+		cout << 0;
+	}
+	else {
+		int answer = 0;
+
+		while (1) {
+			int cnt = 0;
+			int temp = n;
+			while (temp > 0) {
+				if (temp % 2 == 0) {
+					temp /= 2;
+				}
+				else {
+					temp /= 2;
+					cnt++;
+				}
+
+			}
+			if (k >= cnt) {
+				break;
+			}
+
+			n++;
+			answer++;
+		}
+		cout << answer;
+	}
+}
