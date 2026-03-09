84df67295f59e8cfb173c07b1ea419ea83aa1e26
송채강 <herry612@naver.com>
2026-03-10 01:46:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> v(n + 1);
+	vector<int> dp(n + 1, 0);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> v[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			dp[i] = max(dp[i], dp[i - j] + v[j]);
+		}
+	}
+
+	cout << dp[n];
+}
