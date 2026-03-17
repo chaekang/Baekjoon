8b90ab6b3157118fd8658080823cba8d69971844
송채강 <herry612@naver.com>
2026-03-17 10:46:12 +0900

    [Gold V] Title: 탑, Time: 364 ms, Memory: 224488 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,21 +1,23 @@
-# 1월 9
-import sys
+# 스택 - 탑 (백준 골드5)
+# 문제 링크: https://www.acmicpc.net/problem/2493
 
-n = int(sys.stdin.readline())
-tower = list(map(int, sys.stdin.readline().split()))
 
-stack = []
-result = []
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
 
-for i in range(n):
-    while stack:
-        if stack[-1][1] < tower[i]:
-            stack.pop()
-        else:
-            result.append(stack[-1][0] + 1)
-            break
-    if not stack:
-        result.append(0)
-    stack.append([i, tower[i]])
-            
-print(*result)
+    stack = list()
+    ans = list()
+
+    for i in range(n):
+        while stack:
+            if (arr[i] > stack[-1][1]):
+                stack.pop()
+            else:
+                ans.append(stack[-1][0] + 1)
+                break
+        if not stack:
+            ans.append(0)
+        stack.append([i, arr[i]])
+
+    print(*ans)
