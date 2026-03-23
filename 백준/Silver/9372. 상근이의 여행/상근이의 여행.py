369f3bca739b2f6ca3753e03390a859fce393ad8
송채강 <herry612@naver.com>
2026-03-23 11:47:37 +0900

    [Silver IV] Title: 상근이의 여행, Time: 168 ms, Memory: 110744 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,13 @@
+# 트리 - 상근이의 여행 (백준 실버4)
+# 문제 링크: https://www.acmicpc.net/problem/9372
+
+if __name__ == "__main__":
+    t = int(input())
+
+    for _ in range(t):
+        n, m = map(int, input().split())
+
+        for i in range(m):
+            a, b = map(int, input().split())
+
+        print(n-1)
