81237597341abf9633b878439e3ea0bf16b20da4
송채강 <herry612@naver.com>
2026-03-28 13:20:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,18 @@
+# 그리디 - 잃어버린 괄호 (백준 실버2)
+# 문제 링크: https://www.acmicpc.net/problem/1541
+
+if __name__ == "__main__":
+    formula = input().strip()
+
+    parts = formula.split('-')
+    nums = list()
+
+    for p in parts:
+        tmp = sum(map(int, p.split('+')))
+        nums.append(tmp)
+
+    ans = nums[0]
+    for i in range(1, len(nums)):
+        ans -= nums[i]
+
+    print(ans)
