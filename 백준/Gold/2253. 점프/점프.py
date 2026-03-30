b5132750ff7a29bdb3249f0d13511d33aaf4ad56
송채강 <herry612@naver.com>
2026-03-30 19:32:06 +0900

    [Gold IV] Title: 점프, Time: 160 ms, Memory: 123136 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -1,46 +1,37 @@
-# DP - 점프 (백준 골드4)
-# 문제 링크: https://www.acmicpc.net/problem/2253
-
-INF = int(1e9)
+INF = 10**9
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
-
     blocked = set(int(input()) for _ in range(m))
+
+    if 2 in blocked:
+        print(-1)
+        exit()
 
     max_jump = int((2 * n) ** 0.5) + 2
     dp = [[INF] * (max_jump + 1) for _ in range(n + 1)]
-
     dp[1][0] = 0
 
-    if 2 not in blocked:
-        dp[2][1] = 1
-
-
-    for i in range(3, n + 1):
+    for i in range(2, n + 1):
         if i in blocked:
             continue
 
-        for j in range(1, max_jump + 1):
+        limit = int((2 * i) ** 0.5) + 1
+        for j in range(1, limit + 1):
             prev = i - j
             if prev < 1:
-                continue
+                break
 
-            tmp1 = INF
-            tmp2 = INF
-            tmp3 = INF
-            if j-1 > 0:
-                tmp1 = dp[prev][j-1]
-            
-            tmp2 = dp[prev][j]
-            if j+1 <= max_jump:
-                tmp3 = dp[prev][j+1]
+            best = dp[prev][j]
 
-            dp[i][j] = min(tmp1, tmp2, tmp3)+1
+            if j - 1 >= 0:
+                best = min(best, dp[prev][j - 1])
+
+            if j + 1 <= max_jump:
+                best = min(best, dp[prev][j + 1])
+
+            if best != INF:
+                dp[i][j] = best + 1
 
     ans = min(dp[n])
-
-    if ans != INF:
-        print(ans)
-    else:
-        print(-1)
+    print(ans if ans != INF else -1)
