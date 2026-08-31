67e89818c116a8503a7d6da93219d68a8834de30
송채강 <herry612@naver.com>
2026-08-31 17:55:15 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -6,18 +6,16 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
+    
     sort(citations.begin(), citations.end());
     
-    for (int h = 1; h <= citations[citations.size()-1]; h++) {
+    for (int h=1; h<=citations.size(); h++) {
         int count = citations.end() - lower_bound(citations.begin(), citations.end(), h);
-        if (count > h) {
+        
+        if (count >= h) {
             answer = h;
-        }
-        else if (count < h) {
-            break;
         }
         else {
-            answer = h;
             break;
         }
     }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?utm_source=chatgpt.com) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 4.64 MB, 시간: 0.05 ms
+메모리: 4.89 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 25일 21:06:09
+2026년 08월 31일 17:55:15
 
 ### 문제 설명
 
